5249fbfeb4b0f85fd438b8bdbdee51b19ae25fcc
Lalord22 <lalo22296@hotmail.com>
2023-06-21 16:13:56 -0600

    Cleaned app.js from useless code

--- JEGEDAsegurosBackEnd/target/CountriesBackEnd-1.0-SNAPSHOT/js/app.js
+++ JEGEDAsegurosBackEnd/target/CountriesBackEnd-1.0-SNAPSHOT/js/app.js
@@ -3,33 +3,23 @@ class App {
     dom;
     modal; // login modal
     registrationModal; // registration modal
-    updateModal
 
     state; // state variables: if any
 
     preguntas; //
-    clientes;
-    marcas;
-    modelos;
-    categorias;
-    coberturas;
+   
 
     constructor() {
         this.state = {};
         this.dom = this.render();
         this.modal = new bootstrap.Modal(this.dom.querySelector('#modal'));
         this.registrationModal = new bootstrap.Modal(this.dom.querySelector('#registerModal'));
-        this.updateModal = new bootstrap.Modal(this.dom.querySelector('#updateModal'));
         this.dom.querySelector('#apply').addEventListener('click', e => this.login());
         this.dom.querySelector('#registerLink').addEventListener('click', e => this.openRegistrationModal());
         this.renderBodyFiller(); //Cuando la pagina se abre por primera vez, esto imprime el body del website
         this.renderMenuItems(); // Esto carga las opciones en el banner
         this.preguntas = new Preguntas();
-        this.marcas = new Marcas();
-        this.modelos = new Modelos();
-        this.categorias = new Categorias();
-        this.coberturas = new Coberturas();
-        this.clientes = new Clientes();
+       
 
         this.dom.querySelector('#registrationForm').addEventListener('submit', e => {
             e.preventDefault(); // Prevent the default form submission behavior
@@ -46,56 +36,12 @@ class App {
       ${this.renderFooter()}
       ${this.renderModal()}
       ${this.renderRegistrationModal()}
-      ${this.renderUpdateModal()}
     `; //renderMenu esta relacionado al banner
 
         var rootContent = document.createElement('div');
         rootContent.id = 'app';
         rootContent.innerHTML = html;
         return rootContent;
-    }
-
-    renderUpdateModal = () => {
-        return `
-      <div id="updateModal" class="modal fade" tabindex="-1">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title">Actualizar Informacion</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form id="updateForm">
-              <div class="modal-body">
-                <div class="input-group mb-3">
-                  <span class="input-group-text">Nombre</span>
-                  <input type="text" class="form-control" id="updateNombre" name="nombre">
-                </div>
-                <div class="input-group mb-3">
-                  <span class="input-group-text">Clave</span>
-                  <input type="password" class="form-control" id="updateApellido" name="apellido">
-                </div>
-                <div class="input-group mb-3">
-                  <span class="input-group-text">Teléfono</span>
-                  <input type="text" class="form-control" id="updateTelefono" name="telefono">
-                </div>
-                <div class="input-group mb-3">
-                  <span class="input-group-text">Numero de Tarjeta</span>
-                  <input type="text" class="form-control" id="updateDireccion" name="direccion">
-                </div>
-                <div class="input-group mb-3">
-                  <span class="input-group-text">Email</span>
-                  <input type="email" class="form-control" id="updateEmail" name="email">
-                </div>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-                <button type="submit" class="btn btn-primary">Actualizar</button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    `;
     }
 
     renderMenu = () => {   //banner
@@ -224,8 +170,8 @@ class App {
     `;
     }
 
-  renderBodyFiller = () => {
-  var html = `
+    renderBodyFiller = () => {
+        var html = `
     <div id='bodyFiller' style='margin-left: 10%; margin-top:40px; width: 80%; text-align: center; font-size: 1.5em'>
       <div class="banner" style="background-color: lightblue;">
         <h1>Welcome</h1>
@@ -238,12 +184,12 @@ class App {
       
     </div>
   `;
-  this.dom.querySelector('#app>#body').replaceChildren();
-  this.dom.querySelector('#app>#body').innerHTML = html;
-}
+        this.dom.querySelector('#app>#body').replaceChildren();
+        this.dom.querySelector('#app>#body').innerHTML = html;
+    }
 
-renderLogin = () => {
-  var html = `
+    renderLogin = () => {
+        var html = `
     <br>
     <div id="loginForm">
       <div id="loginForm" style="border: 1px solid #ccc; padding: 20px; border-radius: 5px; text-align: center; margin: 0 auto; max-width: 400px;">
@@ -263,15 +209,13 @@ renderLogin = () => {
 
     </div>
   `;
-  return html;
-};
-
-
-
-    renderMenuItems = () => {
+        return html;
+    }
+    ;
+            renderMenuItems = () => {
         var html = '';
         if (globalstate.user === null) {
-            html +=  `
+            html += `
       <li class="nav-item" style="display: flex; justify-content: flex-end;>
         <a class="nav-link" id="infoLink" href="#" data-bs-toggle="modal">
           <img src="images/info.png" alt="Info" style="width: 25px; height: 25px; " />
@@ -315,24 +259,24 @@ renderLogin = () => {
         this.dom.querySelector("#app>#menu #menuItems #addMarca")?.addEventListener('click', e => this.marcasShow());
         this.dom.querySelector("#app>#menu #menuItems #displayClientes")?.addEventListener('click', e => this.clientesShow());
         this.dom.querySelector("#app>#menu #menuItems #loginLink")?.addEventListener('click', () => {
-  // Call the renderLogin method to generate the login form HTML
-  const loginFormHTML = this.renderLogin();
-  
-  // Replace the body content, excluding the banner, with the login form HTML
-  document.querySelector('#app > #body').innerHTML = loginFormHTML;
-  
-  document.querySelector('#apply').addEventListener('click', this.login);
-});
-       this.dom.querySelector("#app>#menu #menuItems #infoLink")?.addEventListener('click', () => {
-  // Call the renderBodyFiller method to generate the body filler content HTML
-  const infoFormHTML = this.renderBodyFiller();
+            // Call the renderLogin method to generate the login form HTML
+            const loginFormHTML = this.renderLogin();
 
-  // Replace the body content, excluding the banner, with the body filler content HTML
-  document.querySelector('#app > #body > .left-half > .mission').innerHTML = infoFormHTML;
-});
+            // Replace the body content, excluding the banner, with the login form HTML
+            document.querySelector('#app > #body').innerHTML = loginFormHTML;
+
+            document.querySelector('#apply').addEventListener('click', this.login);
+        });
+        this.dom.querySelector("#app>#menu #menuItems #infoLink")?.addEventListener('click', () => {
+            // Call the renderBodyFiller method to generate the body filler content HTML
+            const infoFormHTML = this.renderBodyFiller();
+
+            // Replace the body content, excluding the banner, with the body filler content HTML
+            document.querySelector('#app > #body > .left-half > .mission').innerHTML = infoFormHTML;
+        });
 
         this.dom.querySelector("#app>#menu #menuItems #logoutLink")?.addEventListener('click', e => this.logout());
-        this.dom.querySelector("#app>#menu #menuItems #updateLink")?.addEventListener('click', e => this.updateInfo());
+
         this.dom.querySelector("#registerLink")?.addEventListener('click', e => this.registrationModal.show());
         if (globalstate.user !== null) {
             switch (globalstate.user.rol) {
@@ -346,31 +290,6 @@ renderLogin = () => {
     preguntasShow = () => {
         this.dom.querySelector('#app>#body').replaceChildren(this.preguntas.dom);
         this.preguntas.list();
-    }
-
-    marcasShow = () => {
-        this.dom.querySelector('#app>#body').replaceChildren(this.marcas.dom);
-        this.marcas.list();
-    }
-
-    modelosShow = () => {
-        this.dom.querySelector('#app>#body').replaceChildren(this.modelos.dom);
-        this.modelos.list();
-    }
-
-    categoriasShow = () => {
-        this.dom.querySelector('#app>#body').replaceChildren(this.categorias.dom);
-        this.categorias.list();
-    }
-
-    coberturasShow = () => {
-        this.dom.querySelector('#app>#body').replaceChildren(this.coberturas.dom);
-        this.coberturas.list();
-    }
-
-    clientesShow = () => {
-        this.dom.querySelector('#app>#body').replaceChildren(this.clientes.dom);
-        this.clientes.list();
     }
 
     login = async () => {
@@ -409,12 +328,11 @@ renderLogin = () => {
         const bodyElement = this.dom.querySelector('#app>#body');
         bodyElement.innerHTML = '';
     }
-    
-    resetLoginForm = () => {
-      this.dom.querySelector("#identificacion").value = '';
-      this.dom.querySelector("#clave").value = '';
-    }
 
+    resetLoginForm = () => {
+        this.dom.querySelector("#identificacion").value = '';
+        this.dom.querySelector("#clave").value = '';
+    }
 
     logout = () => {
         globalstate.user = null;
@@ -499,121 +417,6 @@ renderLogin = () => {
     registrationModalShow = () => {
         this.registrationModal.show();
     }
-
-    updateInfo = async () => {
-        const updateForm = this.dom.querySelector('#updateForm');
-
-        try {
-            // Make an HTTP GET request to fetch the client data for the currently logged-in client
-            const response = await fetch('http://localhost:8080/JEGEDAsegurosBackEnd/api/clientes/cliente');
-            if (!response.ok) {
-                throw new Error('Failed to fetch client data');
-            }
-
-            const data = await response.json();
-
-            // Populate the update form with the retrieved client data
-            updateForm.querySelector('#updateNombre').value = data.nombre;
-            updateForm.querySelector('#updateApellido').value = data.usuario.clave;
-            updateForm.querySelector('#updateTelefono').value = data.telefono;
-            updateForm.querySelector('#updateDireccion').value = data.datosTarjeta;
-            updateForm.querySelector('#updateEmail').value = data.correo;
-
-            // Show the update modal
-            this.updateModal.show();
-        } catch (error) {
-            console.error('Error fetching client data:', error);
-        }
-
-        // Remove any existing event listeners from the form submission
-        updateForm.removeEventListener('submit', this.updateCliente);
-
-        // Add a new event listener to the form submission
-        updateForm.addEventListener('submit', e => {
-            e.preventDefault(); // Prevent the default form submission behavior
-            this.updateCliente(); // Call the updateCliente method when the form is submitted
-        });
-    }
-
-    updateCliente = async () => {
-  const updateForm = this.dom.querySelector('#updateModal #updateForm');
-  const formData = new FormData(updateForm);
-
-  // Check if any of the required form fields are empty
-  if (
-    formData.get('nombre').trim() === '' ||
-    formData.get('apellido').trim() === '' ||
-    formData.get('telefono').trim() === '' ||
-    formData.get('direccion').trim() === '' ||
-    formData.get('email').trim() === ''
-  ) {
-    alert('Please fill in all the required fields.');
-    return;
-  }
-
-  try {
-    // Make an HTTP GET request to fetch the client data for the currently logged-in client
-    const response = await fetch('http://localhost:8080/JEGEDAsegurosBackEnd/api/clientes/cliente');
-    if (!response.ok) {
-      throw new Error('Failed to fetch client data');
-    }
-
-    const data = await response.json();
-
-    const usuarioData = {
-      cedula: data.usuario.cedula,
-      clave: formData.get('apellido'),
-      tipo: data.usuario.tipo
-    };
-
-    const clienteData = {
-      cedula: data.cedula,
-      nombre: formData.get('nombre'),
-      telefono: formData.get('telefono'),
-      datosTarjeta: formData.get('direccion'),
-      correo: formData.get('email'),
-      usuario: {
-        cedula: data.usuario.cedula,
-        username: data.usuario.username
-      }
-    };
-
-    // Send clienteData to the server for update
-    const updateResponse = await fetch('http://localhost:8080/JEGEDAsegurosBackEnd/api/clientes/update', {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(clienteData)
-    });
-
-    if (updateResponse.ok) {
-      // Update successful
-      const usuarioResponse = await fetch('http://localhost:8080/JEGEDAsegurosBackEnd/api/usuarios/update', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(usuarioData)
-      });
-      if (usuarioResponse.ok) {
-        alert('Update successful.');
-        this.updateModal.hide();
-      } else {
-        // Update failed for usuario
-        alert('An error occurred during the usuario update. Please try again later.');
-      }
-    } else {
-      // Update failed for cliente
-      alert('An error occurred during the cliente update. Please try again later.');
-    }
-  } catch (error) {
-    console.error('Error during update:', error);
-    alert('An error occurred during the update. Please try again later.');
-  }
-};
-
-
 
 }
 
